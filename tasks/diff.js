3b33cfde717b6355222458203a893cb1e67d266e
HTH2 <it@heavy-tuned.de>
2016-12-27 15:49:54 +0100

    fix for new files

--- tasks/diff.js
+++ tasks/diff.js
@@ -1,22 +1,28 @@
 var path = require('path');
 var crypto = require('crypto');
+var fs = require("fs");
 module.exports = function gruntTask(grunt) {
 	'use strict';
 	
 	
 	grunt.registerMultiTask('grunt-copy-different', 'A grunt task for compaaring file content and copying diff files into a target directory', function() {
 		var options = this.data;
-		
+
+		var possibleNewFiles = fs.readdirSync(options.compareWith);
+		var oldFiles = [];
+
 		this.files.forEach(function(f){
 			var files = f.src;
 			var filesToCopy = {};		
 			files.forEach(function(singleFile){
-
 				var fileName = path.basename(singleFile);
+				
+				var file1 = grunt.file.read(singleFile);
+				var file2 = grunt.file.read(options.compareWith+fileName);
+
+				oldFiles[fileName]  = singleFile;
 				if(grunt.file.exists(options.compareWith+fileName)){
-					var file1 = grunt.file.read(singleFile);
-					var file2 = grunt.file.read(options.compareWith+fileName);
-					
+
 					var hash1 = crypto.createHash("md5").update(file1).digest('hex');
 					var hash2 = crypto.createHash("md5").update(file2).digest('hex');
 					if(hash1 !== hash2){
@@ -24,10 +30,16 @@ module.exports = function gruntTask(grunt) {
 					}
 				}
 			});
+			
+			for(var oldFileKey in possibleNewFiles){
+				var oldFile = possibleNewFiles[oldFileKey];
+				if(oldFiles[oldFile] == undefined){
+					filesToCopy[oldFile] = options.compareWith+oldFile;
+				}
+			}
 
 			for(var fileName in filesToCopy){
 				var fileContent = filesToCopy[fileName];
-				console.log(fileName);
 				grunt.file.write(options.destination+fileName, fileContent);
 			}
 		});
